0cb427a9e4bb95a3dcb44ed3e2bee405c9adf826
AKASHKATYAN <akashkatyan2020@gmail.com>
2026-05-16 10:16:54 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.cnnClassifier import logger
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to first log")
+
+STAGE_NAME='Data Ingestion Stage'
+ 
+try:
+    logger.info(f">>>>{STAGE_NAME}started<<<<")    
+    obj=DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>{STAGE_NAME}completed<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e    
